c40466a872b7c50e759af3d9eb4fb8aba410f21c
utk6612 <utkarsh.sriv12@gmail.com>
2017-05-25 10:02:01 +0530

    server changes

--- server/index.js
+++ server/index.js
@@ -1,32 +1,30 @@
 var express = require('express');
-var jsonfile = require('jsonfile')
-//Application Object
+var jsonfile = require('jsonfile');
 
 var app = express();
 var bodyParser = require('body-parser');
 
 
-app.use(bodyParser.json()); // for parsing Application/json
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");//allows client side 
-    //program to access data from server side.
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 
 
-        var file = './db_json/index.json';
-        var data;
-        data=jsonfile.readFileSync(file);
+var file = './db_json/index.json';
+var data;
+data = jsonfile.readFileSync(file);
 
 
-    app.get('/cardData', function (req, res) {
-        res.json(data);
-        res.end();
-    });
+app.get('/cardData', function (req, res) {
+    res.json(data);
+    res.end();
+});
 
 
 app.listen(3000, function () {
